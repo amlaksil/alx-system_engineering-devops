3e467a8d79d9c5f149943dfa2de01d396bbf406d
amlaksil <silamlakdesye@gmail.com>
2023-08-09 03:19:54 +0300

    Number of subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module queries the Reddit API
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Returns the number of subscribers for a given subreddit
+    Args:
+        subreddit: the name of the subreddit to get subscriber
+        count for.
+    """
+    # Get the subreddit's JSON data from the Reddit API.
+    response = requests.get(
+      'https://api.reddit.com/r/{}/about'.format(subreddit))
+    # Check the response status code.
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+        # The number of subscribers is in the "subscribers" key.
+        return data['subscribers']
+    # Invalid subreddits
+    return None
