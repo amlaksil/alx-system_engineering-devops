9aff64cb91ac1fdc2ba0d7dfa4448230b7650c72
amlaksil <silamlakdesye@gmail.com>
2023-08-11 09:49:48 +0300

    Set custome User-Agent

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,20 +8,21 @@ import requests
 
 def top_ten(subreddit):
     """prints the titles of the first 10 hot posts """
+    headers = {
+            'User-Agent': 'Mozilla/5.0'
+            }
     params = {
             'limit': 10
             }
     # Get the subreddit's JSON data from the Reddit API.
     url = 'https://api.reddit.com/r/{}/hot.json'.format(subreddit)
-    response = requests.get(url, params=params, allow_redirects=False)
-    # Check the response status code.
-    if response.status_code != 200:
-        print(None)
-        return
-    dic = response.json()
-    hot_posts = dic['data']['children']
-    if len(hot_posts) is 0:
-        print(None)
-    else:
-        for post in hot_posts:
-            print(post['data']['title'])
+    response = requests.get(
+            url, headers=headers, params=params, allow_redirects=False)
+    if response.status_code == 200:
+        items = response.json()['data']['children']
+        if len(items) == 0:
+            print(None)
+        else:
+            for item in items:
+                print(item['data']['title'])
+    return None
