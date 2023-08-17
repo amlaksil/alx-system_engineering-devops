402879525987904c28280baaf127e24e0dd9d660
amlaksil <silamlakdesye@gmail.com>
2023-08-17 15:04:08 +0300

    Change the ULIMIT value from 15 to 4096 to improve performance

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,14 @@
+# This manifest will allow nginx to use more memory, which may improve performance.
+# However, increasing the ULIMIT value can also increase the risk of security vulnerabilities.
+# This is because a larger ULIMIT value allows a process to use more memory, which can make it
+# more difficult to detect and prevent memory-related attacks.
+exec {'replace':
+    provider => shell,
+    command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+    before   => Exec['restart'],
+}
+
+exec {'restart':
+    provider => shell,
+    command  => 'sudo service nginx restart',
+}
